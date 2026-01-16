42eac7f04cac0f193f73ba6d3474ffd2e3a47d01
Damula <nguyenducminhdang4@gmail.com>
2026-01-16 11:23:01 +0700

    Put ScadaModels into TIA_V20

--- TIA_V20.cs
+++ TIA_V20.cs
@@ -794,4 +794,27 @@ namespace Middleware_console
         #endregion
     }
        
+    #region Data Models
+        public class ScadaScreenModel
+    {
+        public string ScreenName { get; set; }
+        public List<ScadaLayerModel> Layers { get; set; }
+        public List<ScadaItemModel> Items { get; set; }
+    }
+    public class ScadaLayerModel
+    {
+        public string LayerName { get; set; }
+        public List<ScadaItemModel> Items { get; set; }
+    }
+    public class ScadaItemModel
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public bool? EnableCreation { get; set; } = true;
+        public Dictionary<string, object> Properties { get; set; }
+        public Dictionary<string, string> Events { get; set; }
+        public List<ScadaItemModel> Items { get; set; }
+        public string TagName { get; set; }
+    }
+    #endregion
 }
