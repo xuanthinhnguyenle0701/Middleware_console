9b669bfcc88e8d2bc4e34830640d660c2e617484
Damula <nguyenducminhdang4@gmail.com>
2026-01-18 00:01:35 +0700

    Remove System.Linq

--- Navigator.cs
+++ Navigator.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms; // Vẫn giữ để dùng Application.StartupPath nếu cần, nhưng không dùng Dialog
